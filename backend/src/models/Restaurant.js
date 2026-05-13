ae0829357c979a9749805faf9a069e813578c15e
Travis028 <waynetavis123@gmail.com>
2026-05-13 15:57:18 +0300

    Add Restaurant model with UUID and KES currency default

---
+++ backend/src/models/Restaurant.js
@@ -0,0 +1,33 @@
+﻿const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Restaurant = sequelize.define('Restaurant', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    phone: DataTypes.STRING,
+    address: DataTypes.TEXT,
+    currency: {
+        type: DataTypes.STRING,
+        defaultValue: 'KES'
+    },
+    tax_rate: {
+        type: DataTypes.DECIMAL(5,2),
+        defaultValue: 16.00
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = Restaurant;
