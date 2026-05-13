0139f36645d0f831b1f47df7d7ac00962169a601
Travis028 <waynetavis123@gmail.com>
2026-05-13 16:22:14 +0300

    Add model associations between User, Restaurant, Order, MenuItem and Table

---
+++ backend/src/models/index.js
@@ -0,0 +1,27 @@
+﻿const User = require('./User');
+const Restaurant = require('./Restaurant');
+const Order = require('./Order');
+const MenuItem = require('./MenuItem');
+const Table = require('./Table');
+
+User.belongsTo(Restaurant, { foreignKey: 'restaurant_id' });
+Restaurant.hasMany(User, { foreignKey: 'restaurant_id' });
+
+Order.belongsTo(Restaurant, { foreignKey: 'restaurant_id' });
+Order.belongsTo(User, { foreignKey: 'waiter_id' });
+Order.belongsTo(User, { foreignKey: 'cashier_id' });
+Order.belongsTo(Table, { foreignKey: 'table_id' });
+
+MenuItem.belongsTo(Restaurant, { foreignKey: 'restaurant_id' });
+Restaurant.hasMany(MenuItem, { foreignKey: 'restaurant_id' });
+
+Table.belongsTo(Restaurant, { foreignKey: 'restaurant_id' });
+Restaurant.hasMany(Table, { foreignKey: 'restaurant_id' });
+
+module.exports = {
+    User,
+    Restaurant,
+    Order,
+    MenuItem,
+    Table
+};
