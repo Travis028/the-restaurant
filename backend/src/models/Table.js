f3f7abfa20c33db24cf70dc22150c283efc8ff6f
Travis028 <waynetavis123@gmail.com>
2026-05-13 16:15:43 +0300

    Add Table model for restaurant floor management

---
+++ backend/src/models/Table.js
@@ -0,0 +1,28 @@
+﻿const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Table = sequelize.define('Table', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    table_number: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    seating_capacity: {
+        type: DataTypes.INTEGER,
+        defaultValue: 4
+    },
+    status: {
+        type: DataTypes.ENUM('available', 'occupied', 'reserved', 'unavailable'),
+        defaultValue: 'available'
+    },
+    current_order_id: DataTypes.UUID,
+    qr_code: DataTypes.TEXT
+}, {
+    timestamps: true
+});
+
+module.exports = Table;
